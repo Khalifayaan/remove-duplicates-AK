e523f1a936ce31b688b71d91cdd40331802d382a
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-18 15:25:30 +0300

    remove-duplicates assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Good Luck 💪🏾
+
+
+
+function removeDuplicates(array) {
+  return Array.from(new Set(array));
+}
+
+
+const items = [
+  'Apple', 'Grape', 'Banana', 'Watermelon', 'Pineapple', 'Orange', 'Mango', 'Apple', 'Cherry', 'Grape', 'Watermelon', 'Pineapple'
+];
+
+const newItems = removeDuplicates(items);
+console.log(newItems);
